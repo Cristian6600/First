45ce9ba060f3611a90366a979f365c203a59aafb
Cristian6600 <cristian6600@hotmail.es>
2021-06-01 14:14:23 -0500

    in1

--- firstsource/applications/inventario/models.py
+++ firstsource/applications/inventario/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import CharField
+from django.db.models.fields import BooleanField, CharField
 from applications.users.models import User, Areas
 
 
@@ -33,8 +33,23 @@ class Inventario(models.Model):
     Ubicacion = models.ForeignKey(Areas, on_delete=models.CASCADE)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     Licencia = models.ManyToManyField(Licencia)
-    Observacion = models.TextField(max_length=50)
-    ip = models.GenericIPAddressField()
+    Observacion = models.TextField(max_length=250)
+    ip = models.CharField(max_length=20, blank=True)
+    Mac = models.CharField(max_length=20)
+    Ram = models.CharField(max_length=6, blank=True)
+    Disco = models.CharField(max_length=30, blank=True)
+    Procesador = models.CharField(max_length=15, blank=True)
+    Usb = models.BooleanField(default=False, blank=True)
+    Hdmi = models.BooleanField(default=False, blank=True)
+    Vga = models.BooleanField(default=False, blank=True)
+    Bloq_ex = BooleanField(default=False, blank=True, verbose_name='Bloqueo de extraibles')
+    bloq_pa= BooleanField(default=False, blank=True, verbose_name='Bloqueo de panel de control')
+    #in_cont = models.CharField(max_length=2, verbose_name='Intento contraseñas', blank=True)
+
+    
+
+    
+
 
 
     
@@ -56,6 +71,7 @@ class inmobiliario(models.Model):
     Area= models.ForeignKey(Areas, on_delete=models.CASCADE)
     Piso= models.IntegerField()
     Observacion = models.TextField(max_length=30)
+    
 
     def __str__(self):
         return self.serial
